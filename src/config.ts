d595eeace23abaf2a477534d5e562f74f5770cd7
Shreeram S <testinggrounds221.com>
2026-06-10 18:15:10 +0530

    config

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,11 @@
 export const ALLOWED_ADMIN_EMAILS = [
-  'sb14backup@gmail.com'
+	'sb14backup@gmail.com'
 ];
 
 export const MASTER_KEYS = [
-  'SharanWedsAnnapurna2026',
-  'SharanKAnnapurna',
-  'Annapurna2026',
-  'sb14rsvp'
+	'SharanWedsAnnapurna2026',
+	'SharanKAnnapurna',
+	'Annapurna2026',
+	'sb14rsvp',
+	'shanu'
 ];
